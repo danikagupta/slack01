2bdde12d33e325635268c202282d186b17ac3c0e
danikagupta <74515001+danikagupta@users.noreply.github.com>
2024-07-04 16:23:15 +0000

    Added access key

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,61 +57,61 @@ def handle_selection(title, url):
     st.write(f"Selected Title: {title}")
     st.write(f"YouTube URL: {url}")
 
-css = """
-<style>
-    .stColumn {
-        border: 1px solid #ddd;
-        padding: 10px;
-        text-align: left;
-    }
-    .stButton button {
-        width: 100%;
-    }
-</style>
-"""
+def authenticate():
+    st.title("Authentication Required")
+    password = st.text_input("Enter the access key:", type="password")
+    if password == st.secrets["ACCESS_KEY"]:
+        st.session_state["authenticated"] = True
+        st.experimental_rerun()
+    elif password:
+        st.error("Invalid secret key")
 
-# Inject CSS with Markdown
-st.markdown(css, unsafe_allow_html=True)
+def main_page():
+    # Streamlit app layout
+    st.sidebar.title('Slack Message Fetcher')
 
-# Streamlit app layout
-st.sidebar.title('Slack Message Fetcher')
-
-# Fetch and display available channels
-if 'channels' not in st.session_state:
-    st.session_state.channels = fetch_channels()
-channels = st.session_state.channels
-channel_options = {channel['name']: channel['id'] for channel in channels}
-channel_name = 'session-notifications'
-channel_id = channel_options[channel_name]
-
-
-start_date = st.sidebar.date_input('Start date', datetime.now())
-start_time = st.sidebar.time_input('Start time', datetime.now().time())
-end_date = st.sidebar.date_input('End date', datetime.now())
-end_time = st.sidebar.time_input('End time', datetime.now().time())
-
-start_datetime = datetime.combine(start_date, start_time)
-end_datetime = datetime.combine(end_date, end_time)
-
-if st.sidebar.button('<Fetch Messages>'):
+    # Fetch and display available channels
+    if 'channels' not in st.session_state:
+        st.session_state.channels = fetch_channels()
+    channels = st.session_state.channels
+    channel_options = {channel['name']: channel['id'] for channel in channels}
+    channel_name = 'session-notifications'
     channel_id = channel_options[channel_name]
-    messages = fetch_messages(channel_id, start_datetime, end_datetime)
-    st.session_state.df=get_df_from_messages(messages)
 
-if 'df' in st.session_state:
-    event=st.dataframe(
-        st.session_state.df,
-        on_select='rerun',
-        hide_index=True,
-        selection_mode='multi-row'
-    )
 
-    print(f"Event: {event}")
-    if len(event.selection['rows'])>0:
-        print(f"Selected Rows: {event.selection['rows']}")
-        row_index=event.selection['rows'][0]
-        title=st.session_state.df.iloc[row_index]['Title']
-        url=st.session_state.df.iloc[row_index]['YouTube URL']
-        handle_selection(title, url)
+    start_date = st.sidebar.date_input('Start date', datetime.now())
+    start_time = st.sidebar.time_input('Start time', datetime.now().time())
+    end_date = st.sidebar.date_input('End date', datetime.now())
+    end_time = st.sidebar.time_input('End time', datetime.now().time())
 
-        
+    start_datetime = datetime.combine(start_date, start_time)
+    end_datetime = datetime.combine(end_date, end_time)
+
+    if st.sidebar.button('<Fetch Messages>'):
+        channel_id = channel_options[channel_name]
+        messages = fetch_messages(channel_id, start_datetime, end_datetime)
+        st.session_state.df=get_df_from_messages(messages)
+
+    if 'df' in st.session_state:
+        event=st.dataframe(
+            st.session_state.df,
+            on_select='rerun',
+            hide_index=True,
+            selection_mode='multi-row'
+        )
+
+        print(f"Event: {event}")
+        if len(event.selection['rows'])>0:
+            print(f"Selected Rows: {event.selection['rows']}")
+            row_index=event.selection['rows'][0]
+            title=st.session_state.df.iloc[row_index]['Title']
+            url=st.session_state.df.iloc[row_index]['YouTube URL']
+            handle_selection(title, url)
+
+if __name__ == '__main__':
+    if "authenticated" not in st.session_state:
+        st.session_state["authenticated"] = False
+    if st.session_state["authenticated"]:
+        main_page()
+    else:
+        authenticate()     
